3fed00d6966f4373c5697d3388405bbc56bc17a4
HalimaSaadia <halimasadia9666@gmail.com>
2024-11-06 20:05:51 +0600

    resolve problem-2

---
+++ problem-2.ts
@@ -0,0 +1,12 @@
+{
+  const removeDuplicates = (numbers: number[]): number[] => {
+    let uniqueNumbers: number[] = [];
+    numbers.forEach((num) => {
+      if (uniqueNumbers.indexOf(num) === -1) {
+        uniqueNumbers.push(num);
+      }
+    });
+    return uniqueNumbers;
+  };
+  console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5, 5, 5, 7, 8, 8]));
+}
